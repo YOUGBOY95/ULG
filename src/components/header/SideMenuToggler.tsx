1091cf0e51ed31583aa914b12c0c55ba237a5ad6
Pinard Nathan <86224840+YOUGBOY95@users.noreply.github.com>
2023-08-19 11:14:42 +0200

    code source v10

--- src/components/header/SideMenuToggler.tsx
+++ src/components/header/SideMenuToggler.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useState } from "react";
 import { Icon } from "@iconify/react";
 import closeIcon from "../../assets/img/close.png"; // Chemin vers votre icône
+import Logo from "../../assets/img/logo.png"; // Chemin vers le logo
+import optilience from "../../assets/img/optilience.png";
 
 const SideMenuToggler: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,11 +34,27 @@ const SideMenuToggler: FC = () => {
             >
               <img src={closeIcon} alt="Close" className="h-6 w-6" />
             </span>
-            <ul className="space-y-2">
-              <li>Événements 1</li>
-              <li>Événements 2</li>
-              {/* Ajoutez d'autres éléments de menu ici */}
-            </ul>
+
+            <a href="/" style={{ position: "absolute", top: "1.2rem", left: "1rem" }}>
+              {/* Utilisation de Link pour le lien */}
+              <img src={Logo} alt="Logo" className="w-10 h-10 cursor-pointer" />
+            </a>
+    
+            <a href="/" style={{ position: "absolute", top: "-0.05rem", left: "3.5rem" }}>
+  <img src={optilience} alt="optilience" className="w-20 h-20 cursor-pointer" />
+</a>
+            <div className="flex flex-col items-center justify-center h-full">
+              <ul className="space-y-2 text-center">
+                <li>Événements 1</li>
+                <br></br>
+                <li>Événements 2</li>
+                <br></br>
+                <li>Événements 3</li>
+                <br></br>
+                <li>Événements 4</li>
+                {/* Ajoutez d'autres éléments de menu ici */}
+              </ul>
+            </div>
           </div>
         </div>
       )}
