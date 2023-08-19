93499a637ebbdd5b43e44233ecc107e4b67549a9
Pinard Nathan <86224840+YOUGBOY95@users.noreply.github.com>
2023-08-19 21:55:05 +0200

    code source v10

--- src/components/header/SideMenuToggler.tsx
+++ src/components/header/SideMenuToggler.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useState } from "react";
 import { Icon } from "@iconify/react";
-import closeIcon from "../../assets/img/close.png"; // Chemin vers votre icône
-import Logo from "../../assets/img/logo.png"; // Chemin vers le logo
+import Logo from "../../assets/img/logo.png";
 import optilience from "../../assets/img/optilience.png";
 
 const SideMenuToggler: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedLink, setSelectedLink] = useState<number | null>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -14,6 +14,19 @@ const SideMenuToggler: FC = () => {
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
+
+  const handleLinkClick = (index: number) => {
+    setSelectedLink(index);
+    setIsMenuOpen(false);
+  };
+
+  const links = [
+    { label: "ÉVÉNEMENTS", target: "événements" },
+    { label: "INSCRIPTIONS ÉVÉNEMENTS", target: "inscriptions" },
+    { label: "NEWS", target: "news" },
+    { label: "ULG ?", target: "ulg" },
+    { label: "PLUS", target: "plus" },
+  ];
 
   return (
     <div className="relative">
@@ -24,40 +37,43 @@ const SideMenuToggler: FC = () => {
         <Icon icon="ic:baseline-menu" className="text-[#fcfcfc]" height={24} />
       </span>
       
-      {isMenuOpen && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-black p-4 w-full h-full sm:w-[390px] sm:h-[844px] text-white relative">
-            <span
-              onClick={closeMenu}
-              className="absolute top-4 right-4 cursor-pointer"
-              style={{ top: "1.8rem", right: "1.8rem" }} // Ajustez ces valeurs
-            >
-              <img src={closeIcon} alt="Close" className="h-6 w-6" />
-            </span>
+      <div
+        className={`fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center transition-transform duration-300 ${isMenuOpen ? "-translate-x-0" : "translate-x-full"} right-0`}
+      >
+        <div className="bg-black p-4 w-full h-full sm:w-[390px] sm:h-[844px] text-white relative">
+          <span
+            onClick={closeMenu}
+            className="absolute top-4 right-4 cursor-pointer"
+            style={{ top: "1.8rem", right: "1.8rem" }}
+          >
+            <Icon icon="ic:baseline-close" className="text-[#fcfcfc]" height={24} />
+          </span>
 
-            <a href="/" style={{ position: "absolute", top: "1.2rem", left: "1rem" }}>
-              {/* Utilisation de Link pour le lien */}
-              <img src={Logo} alt="Logo" className="w-10 h-10 cursor-pointer" />
-            </a>
+          <a href="/" style={{ position: "absolute", top: "1.2rem", left: "1rem" }}>
+            <img src={Logo} alt="Logo" className="w-10 h-10 cursor-pointer" />
+          </a>
     
-            <a href="/" style={{ position: "absolute", top: "-0.05rem", left: "3.5rem" }}>
-  <img src={optilience} alt="optilience" className="w-20 h-20 cursor-pointer" />
-</a>
-            <div className="flex flex-col items-center justify-center h-full">
-              <ul className="space-y-2 text-center">
-                <li>Événements 1</li>
-                <br></br>
-                <li>Événements 2</li>
-                <br></br>
-                <li>Événements 3</li>
-                <br></br>
-                <li>Événements 4</li>
-                {/* Ajoutez d'autres éléments de menu ici */}
-              </ul>
-            </div>
+          <a href="/" style={{ position: "absolute", top: "-0.05rem", left: "3.5rem" }}>
+            <img src={optilience} alt="optilience" className="w-20 h-20 cursor-pointer" />
+          </a>
+          
+          <div className="flex flex-col items-center justify-center h-full">
+            <ul className="space-y-10 text-center">
+              {links.map((link, index) => (
+                <li key={link.label} className="menu-link">
+                  <a
+                    href={`#${link.target}`}
+                    onClick={() => handleLinkClick(index)}
+                    className={selectedLink === index ? "selected-link" : ""}
+                  >
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
