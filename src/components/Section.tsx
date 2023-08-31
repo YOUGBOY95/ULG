1a23c439ae887bd62b3512da69f0790e930bd3c7
Pinard Nathan <86224840+YOUGBOY95@users.noreply.github.com>
2023-08-31 19:48:15 +0200

    modification de la barre transversale

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -10,12 +10,7 @@ const Section: FC<{
 px-[5%] lg:px-[7.3%] 
 ${className ? className : ""}`}
   >
-    <span
-      className={`absolute h-full top-0 border-l
-    ${blackVLine ? "border-slate-900/40" : "border-slate-50/50"} z-10 
-    left-[5%] lg:left-[7.3%]
-    `}
-    ></span>
+   
     {children}
   </section>
 );
